90aee29998ae1c93562730200d5c9d01b68c11d8
minast1 <edmarfo2@gmail.com>
2022-05-08 23:19:05 +0000

    added google iframe

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import DepartureBoardOutlinedIcon from "@mui/icons-material/DepartureBoardOutlined";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -14,11 +13,8 @@ import { useNavigate } from "@remix-run/react";
 import Paper from "@mui/material/Paper";
 import styles from "~/styles/carousel.min.css";
 import Slider from "~/components/Slider";
-const sessions = [
-  { label: "Morning", value: "MORNING" },
-  { label: "Afternoon", value: "AFTERNOON" },
-  { label: "Evening", value: "EVENING" },
-];
+import Alert from "@mui/material/Alert";
+
 const IndexPage = () => {
   //const data = useOutletContext<dataType>();
 
@@ -29,6 +25,26 @@ const IndexPage = () => {
   return (
     <>
       <Slider />
+      <Card sx={{ mb: 5, mt: 2, backgroundColor: "white" }} elevation={15}>
+        <Box display="flex" alignItems="center" ml={2} mt={2}>
+          <Typography variant="body1">Where do You Want to go...?</Typography>
+          <Box flexGrow={1} />
+        </Box>
+
+        <CardContent sx={{ borderTop: "1px solid lightgray" }}>
+          <iframe
+            title="mytitle"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.3629524911203!2d-0.1680644857560083!3d5.660534334164422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf9c9435692819%3A0xe90ce65988c21ad6!2sUniversity%20of%20Professional%20Studies%2C%20Accra%20(UPSA)!5e0!3m2!1sen!2sgh!4v1652051332320!5m2!1sen!2sgh"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            //allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </CardContent>
+        <CardActions disableSpacing></CardActions>
+      </Card>
     </>
   );
 };
